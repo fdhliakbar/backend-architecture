9c1d435d3ef3cae7242d4f989d744f6dbecf6257
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-18 15:03:32 +0700

    upload some files in shorthand folders

---
+++ Javascript/Shorthand/objectEntries.js
@@ -0,0 +1,10 @@
+const credits = { producer: "John", director: "Jane", assistant: "Peter" };
+const arr = Object.entries(credits);
+console.log(arr);
+
+/** Output:
+[ [ 'producer', 'John' ],
+  [ 'director', 'Jane' ],
+  [ 'assistant', 'Peter' ]
+]
+**/
