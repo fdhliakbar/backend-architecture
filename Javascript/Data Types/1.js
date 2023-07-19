d22a9af088046fb4c8beaaef585896744e0c7edb
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-19 20:34:40 +0700

    upload 1.js in data types folders

---
+++ Javascript/Data Types/1.js
@@ -0,0 +1,40 @@
+/*******************************************
+
+    Javascript has 8 Data Types
+    1. String
+    2. Number
+    3. Bigint
+    4. Boolean
+    5. Undefined
+    6. Null
+    7. Symbol
+    8. Object
+
+    The Object Datatype
+    The object data type can contain:
+
+    1. An object
+    2. An array
+    3. A date
+*******************************************/
+
+// Object
+const person = {
+  firstName: "Muhamad",
+  middleName: "Fadhli",
+  lastName: "Akbar",
+};
+
+// Array
+const numbers = [4, 7, 2, 6, 3];
+
+// Date
+const date = new Date("2023-07-19");
+
+let number; // Now number is undefined
+number = 10; // Now number is a number
+number = "ten"; // Now number is a string value
+
+let islandName = '"Skyplea island" which is in one piece';
+
+console.log(islandName);
