c34db1d48ec5ba3be6ffb27b6f9382b076feafc0
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-19 21:16:58 +0700

    rename an func file to func1

---
+++ Javascript/Loop with Func/loop.js
@@ -0,0 +1,31 @@
+let number = 3;
+let count = 1;
+
+for (let i = 0; i <= number; i++) {
+  if (i == 3) {
+    console.log(i);
+  }
+}
+
+for (; count <= number; count++) {
+  console.log(count);
+}
+
+for (; count <= number; ) {
+  console.log(count);
+  count++;
+}
+
+/* The foor in loop
+
+  for (key in object)
+
+  for (variable in array)
+
+*/
+
+// while loop
+while (count <= number) {
+  console.log(count);
+  count++;
+}
