6e69d4c2064a06b69060b3b0dd20135490c1c94a
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-19 20:47:27 +0700

    update 1.js

--- Javascript/Data Types/1.js
+++ Javascript/Data Types/1.js
@@ -23,6 +23,8 @@ const person = {
   firstName: "Muhamad",
   middleName: "Fadhli",
   lastName: "Akbar",
+  age: 32,
+  anime: "yes",
 };
 
 // Array
@@ -35,6 +37,21 @@ let number; // Now number is undefined
 number = 10; // Now number is a number
 number = "ten"; // Now number is a string value
 
+// double quote
 let islandName = '"Skyplea island" which is in one piece';
 
-console.log(islandName);
+// Exponenntial Notation
+
+let test1 = 1e2; // 100
+let test2 = 1e1; // 10
+let test3 = 10e2; // 0.10
+let test4 = 92e-4; // 0.0092
+
+for (let i = 1; i <= test1; i++) {
+  if (i == 100) {
+    console.log("Ends to ", i);
+    console.log("Variable test4 result: ", test4);
+  }
+}
+
+console.log(`Do you like anime? ${person.anime}`);
