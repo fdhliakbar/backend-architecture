7c54e6c2bf7dda0320592effade33e5b4e9853b6
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-21 12:09:44 +0700

    deleted some folders

--- Javascript/Loop with Func/func2.js
+++
@@ -1,8 +0,0 @@
-// Array.forEach = a callback function
-
-const numbers = [10, 20, 30, 40, 50];
-numbers.forEach(printNumbers);
-
-function printNumbers(value, index, array) {
-  console.log(value, index, array);
-}
--- Javascript/OOP/1.js
+++
@@ -1,5 +0,0 @@
-const human = { type: "mamalia", weapon: "brain", color: "white" };
-
-for (let i = 0; i < 3; i++) {
-  console.log(human.weapon);
-}
