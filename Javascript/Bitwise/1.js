f43e37bc03658c39010713829abb786f3112b8ef
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-21 12:08:40 +0700

    upload

---
+++ Javascript/Bitwise/1.js
@@ -0,0 +1,10 @@
+// Operator typeof(operand)
+
+let x = true;
+//console.log(typeof x);
+
+var value; //undefined
+value = null; //null
+
+var angka = prompt("masukkan angka : ");
+console.log("angka : ", angka);
---
+++ Javascript/OOP/1.js
@@ -0,0 +1,5 @@
+const human = { type: "mamalia", weapon: "brain", color: "white" };
+
+for (let i = 0; i < 3; i++) {
+  console.log(human.weapon);
+}
