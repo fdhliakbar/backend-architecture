71cb46ef37aa0628444be5e9d98ec6c3ca3e15ad
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2023-07-21 19:27:59 +0700

    upload nullish coalescing file

---
+++ Javascript/Basic/ternary.js
@@ -0,0 +1,12 @@
+const value = 82;
+
+if (value >= 80) {
+  //statement
+} else {
+  //statement
+}
+
+// with ternary operator
+let congrast = value >= 80 ? "Congratulations" : "Sorry";
+
+console.log(congrast);
